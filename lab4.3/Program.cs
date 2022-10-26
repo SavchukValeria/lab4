e0553523d9c5b7b4d88278d41b03924a0b75c2e3
SavchukValeria <V.Savchuk_FIT_12_21_B_d@knute.edu.ua>
2022-10-26 23:34:57 +0300

    1
    1

---
+++ lab4.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4._3
+{
+  class Printer
+  { 
+        static void Main(params string[] value)
+        {
+            Console.SetCursorPosition(0,0);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Привiт");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Мене звати Валерiя");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("А тебе?");
+
+            Console.ReadKey();
+        }
+  }
+   
+}
